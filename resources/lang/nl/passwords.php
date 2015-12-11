5035311845ae84d4d47cd3bb8d9d446370c2ca7a
Sander de Kroon <git@dekroon.xyz>
2015-12-11 15:40:50 +0100

    Added Localization

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten minimaal zes tekens bevatten en overeenkomen.',
+    'reset' => 'Het wachtwoord is opnieuw ingesteld!',
+    'sent' => 'Er is een e-mail verstuurd met een link om het wachtwoord te wijzigen.',
+    'token' => 'Het wachtwoord reset token is ongeldig.',
+    'user' => "Er is geen gebruiker bekend met dat e-mailadres.",
+
+];

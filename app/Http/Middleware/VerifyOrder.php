5cfdf2a70f655b7b581a3965bb3a7bd55ee23d02
Sander de Kroon <git@dekroon.xyz>
2015-12-14 13:32:41 +0100

    Orders don't carry over from user to guest and merge correctly
    Before this fix, it was possible to logout and carry the order (and
    orderlines) to a guest account. Now the session gets flushed on logout,
    so that no order gets carried around.
    When logging in, the order a guest makes gets properly merged with the
    order that already existed in the user account. The order a guest has
    made while not logged in, is the one that gets applied to the user
    order.

--- app/Http/Middleware/VerifyOrder.php
+++ app/Http/Middleware/VerifyOrder.php
@@ -21,9 +21,23 @@ class VerifyOrder
     {
         $sessionOrder = Order::findOrFail($sessionID);
         $sessionOrderLines = $sessionOrder->orderlines()->get();
+        $userOrder = Order::find($orderID);
+        $userOrderLines = $userOrder->orderlines()->get();
 
         if (count($sessionOrderLines)) {
             foreach ($sessionOrderLines as $sessionOrderLine) {
+                if (count($userOrderLines)) {
+                    foreach ($userOrderLines as $userOrderLine) {
+                        if ($userOrderLine->product_id == $sessionOrderLine->product_id) {
+                            $userOrderLine->amount = $sessionOrderLine->amount;
+                            $userOrderLine->save();
+                            $sessionOrderLine->delete();
+                        } else {
+                            $sessionOrderLine->order_id = $orderID;
+                            $sessionOrderLine->save();
+                        }
+                    }
+                }
                 $sessionOrderLine->order_id = $orderID;
                 $sessionOrderLine->save();
             }
@@ -84,7 +98,7 @@ class VerifyOrder
             $sessionOrderID = $request->session()->get('order_id');
 
             if ($databaseOrderID->id != $sessionOrderID) {
-                $this->mergeOrders($databaseOrderID, $sessionOrderID, $request);
+                $this->mergeOrders($databaseOrderID->id, $sessionOrderID, $request);
             }
         }
 

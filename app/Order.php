2f1b32fd1c1a3a4bef131d973897c48c004f6951
Sander de Kroon <git@dekroon.xyz>
2015-12-15 09:53:17 +0100

    Added status relationship

--- app/Order.php
+++ app/Order.php
@@ -29,4 +29,9 @@ class Order extends Model
     {
         return $this->hasMany('App\Orderline');
     }
+
+    public function status()
+    {
+        return $this->hasOne('App\Status');
+    }
 }

13e6e876e46951e6b4bc693b12cb5c3347067131
Sander de Kroon <git@dekroon.xyz>
2016-02-04 21:15:43 +0100

    Added e-mail templates for ready and cancelled orders

---
+++ resources/views/emails/order-cancelled.blade.php
@@ -0,0 +1,5 @@
+Hi <?php echo $user->name; ?>!
+
+Je bestelling is zojuist geannuleerd.
+
+Groet, Krat en Klaar.
---
+++ resources/views/emails/order-ready.blade.php
@@ -0,0 +1,6 @@
+Hi <?php echo $user->name; ?>!
+
+Je bestelling is zojuist klaar gezet. 
+Vanaf nu kun je je bestelling ophalen op het ophaalpunt.
+
+Groet, Krat en Klaar.

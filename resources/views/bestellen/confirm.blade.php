b6c4b991b769d0e5e3885d5a170e86b86828577a
Sander de Kroon <git@dekroon.xyz>
2016-02-04 21:18:59 +0100

    Added styling to order confirm page

--- resources/views/bestellen/confirm.blade.php
+++ resources/views/bestellen/confirm.blade.php
@@ -3,13 +3,27 @@
 @section('content')
 
 	<div class="row">
-		<h1>Bestelling geslaagd!</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div class="small-12 columns">
+			<div class="skew orange">
+				<h2>Bestelling geslaagd</h2>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-10 large-5 small-centered columns">
+			<div class="panel">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="small-6 columns small-centered">
+					<a class="light-green button expanded" href="{{URL::action('HomeController@index')}}">Ga verder</a>
+				</div>
+			</div>
+		</div>
 	</div>
 	
 	

e246fee1d0a40a6114b43cee6e2dec47003190be
Sander de Kroon <git@dekroon.xyz>
2016-02-04 21:18:21 +0100

    Updated seeder to insert current year

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,7 +14,7 @@ class ProductsTableSeeder extends Seeder
     {
         $ratatouille = new App\Product;
         $ratatouille->week_no = date('W');
-        $ratatouille->year = 2015;
+        $ratatouille->year = date('Y');
         $ratatouille->name = 'Ratatouille';
         $ratatouille->description = 'Ratatouille is een Frans gerecht van gestoofde groenten, dat vooral in de Provence veel wordt bereid.';
         $ratatouille->city_id = 1;
@@ -22,7 +22,7 @@ class ProductsTableSeeder extends Seeder
 
         $spaghetti = new App\Product;
         $spaghetti->week_no = date('W');
-        $spaghetti->year = 2015;
+        $spaghetti->year = date('Y');
         $spaghetti->name = 'Spaghetti';
         $spaghetti->description = 'Lorem ipsum.';
         $spaghetti->city_id = 1;
@@ -30,7 +30,7 @@ class ProductsTableSeeder extends Seeder
 
         $visgerecht = new App\Product;
         $visgerecht->week_no = date('W');
-        $visgerecht->year = 2015;
+        $visgerecht->year = date('Y');
         $visgerecht->name = 'Visgerecht';
         $visgerecht->description = 'Lorem ipsum.';
         $visgerecht->city_id = 2;
@@ -38,7 +38,7 @@ class ProductsTableSeeder extends Seeder
 
         $testgerecht = new App\Product;
         $testgerecht->week_no = date('W');
-        $testgerecht->year = 2015;
+        $testgerecht->year = date('Y');
         $testgerecht->name = 'Test gerecht';
         $testgerecht->description = 'Lorem ipsum.';
         $testgerecht->city_id = 2;

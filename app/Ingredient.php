d1b949414f5005f8e85a240cc14128c94f6a94e7
Sander de Kroon <git@dekroon.xyz>
2016-02-04 21:14:26 +0100

    Added method to determine column statuses

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
@@ -19,5 +20,22 @@ class Ingredient extends Model
     public function products()
     {
         return $this->belongsToMany('App\Product', 'ingredients_in_products', 'ingredient_id', 'product_id');
+    }
+
+    /**
+     * Get the possible statuses for either the column 'unit' or 'type'.
+     *
+     * @param  String $field The column name
+     * @return Array         An array with the possible statuses
+     */
+    public static function getPossibleStatuses($field)
+    {
+        $type = DB::select(DB::raw('SHOW COLUMNS FROM ingredients WHERE Field = "'.$field.'"'))[0]->Type;
+        preg_match('/^enum\((.*)\)$/', $type, $matches);
+        $values = array();
+        foreach (explode(',', $matches[1]) as $value) {
+            $values[] = trim($value, "'");
+        }
+        return $values;
     }
 }
